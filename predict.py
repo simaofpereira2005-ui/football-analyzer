928b445b5b9f6947df77bd721ca8a47bbc388068
Simao <simaofpereira2005@gmail.com>
2026-06-22 01:43:33 +0100

    finaliza o motor de probabilidades de vitoria, empate ou loss

--- predict.py
+++ predict.py
@@ -28,6 +28,30 @@ def poisson_prob(lambd, golos):
     """Aplica a fórmula matemática da Distribuição de Poisson."""
     return (math.exp(-lambd) * (lambd**golos)) / math.factorial(golos)
 
+def calcular_probabilidades_jogo(lambda_casa, lambda_fora, max_golos=5):
+    """Calcula as % de Vitória, Empate e Derrota somando a matriz de resultados."""
+    prob_vitoria_casa = 0.0
+    prob_empate = 0.0
+    prob_vitoria_fora = 0.0
+    
+    # Criamos a nossa grelha de combinações (de 0 a 5 golos para cada equipa)
+    for golos_casa in range(max_golos + 1):
+        for golos_fora in range(max_golos + 1):
+            
+            prob_c = poisson_prob(lambda_casa, golos_casa)
+            prob_f = poisson_prob(lambda_fora, golos_fora)
+            
+            prob_resultado_exato = prob_c * prob_f
+            
+            if golos_casa > golos_fora:
+                prob_vitoria_casa += prob_resultado_exato
+            elif golos_casa == golos_fora:
+                prob_empate += prob_resultado_exato
+            else:
+                prob_vitoria_fora += prob_resultado_exato
+                
+    return prob_vitoria_casa * 100, prob_empate * 100, prob_vitoria_fora * 100
+
 if __name__ == "__main__":
     dados_stats = stats.obter_estatisticas_completas()
     
@@ -36,9 +60,12 @@ if __name__ == "__main__":
     
     lambda_A, lambda_B = calcular_lambda(equipa_A, equipa_B, dados_stats)
     
-    print(f"--- EXPECTATIVA DE GOLOS (xG) ---")
-    print(f"{equipa_A} (Casa): {lambda_A:.2f} golos esperados")
-    print(f"{equipa_B} (Fora): {lambda_B:.2f} golos esperados\n")
-    
-    prob_arsenal_2_golos = poisson_prob(lambda_A, 2) * 100
-    print(f"Probabilidade de o {equipa_A} marcar exatamente 2 golos: {prob_arsenal_2_golos:.1f}%")
+    if lambda_A and lambda_B:
+        vit_casa, empate, vit_fora = calcular_probabilidades_jogo(lambda_A, lambda_B)
+        
+        print(f"--- PREVISÃO DE JOGO: {equipa_A} vs {equipa_B} ---")
+        print(f"Vitória {equipa_A}: {vit_casa:.1f}%")
+        print(f"Empate: {empate:.1f}%")
+        print(f"Vitória {equipa_B}: {vit_fora:.1f}%")
+    else:
+        print("Dados insuficientes para estas equipas.")
